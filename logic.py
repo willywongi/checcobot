1882bedd48a150c14f8d707478918451087141b4
Francesco Pongiluppi <pongiluppi.f@mmfg.it>
2017-07-23 17:21:20 +0200

    Fix alle logiche di risposta

--- logic.py
+++ logic.py
@@ -33,7 +33,16 @@ def main(update):
     text = update['message'].get('text')
     text_response = None
     if text:
-        if chat['type'] == 'private':
+        # rispondi solo con certe keyword
+        try:
+            rss = next(r[1] for r in responses if r[0].match(text.lower()))
+        except StopIteration:
+            rss = ()
+        if rss:
+            c = CHAT_COUNTER.get(chat_id, 0) + 1
+            CHAT_COUNTER[chat_id] = c
+            text_response = rss[len(rss) % c]
+        if chat['type'] == 'private' and not text_response:
             # rispondi con smalltalk di api.ai
             call_telegram("sendChatAction", chat_id=chat_id, action="typing")
             res = call_apiai("query", query=text, sessionId=chat_id, lang="it")
@@ -42,16 +51,6 @@ def main(update):
                 text_response = res['result']['fulfillment']['speech']
             except KeyError:
                 text_response = "Non ti capisco"
-        if chat['type'] == 'group':
-            # rispondi solo con certe keyword
-            try:
-                rss = next(r[1] for r in responses if r[0].match(text.lower()))
-            except StopIteration:
-                rss = ()
-            if rss:
-                c = CHAT_COUNTER.get(chat_id, 0) + 1
-                CHAT_COUNTER[chat_id] = c
-                text_response = rss[len(rss) % c]
     
     if text_response:
         call_telegram("sendMessage", chat_id=chat_id, text=text_response)
