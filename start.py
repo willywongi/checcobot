401c002205d93f50ce61050446028bb162d9c133
Francesco Pongiluppi <pongiluppi.f@mmfg.it>
2017-07-26 14:24:01 +0200

    First webhook implementation.

--- start.py
+++ start.py
@@ -1,9 +1,20 @@
 # coding: utf-8
 import logging
+import json
 import sys
 
 from communication import get_updates
 from logic import main
+
+def application(environ, start_response):
+	update = json.loads(environ['wsgi.input'].read())
+	main(update)
+	status = '200 OK'
+	response_body = "OK"
+	header = [("Content-Type", "text/plain"), ("Content-Length", "2")]
+	start_response(status, header)
+	return [response_body]
+
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(asctime)s (%(thread)s) %(levelname)s - %(message)s", stream=sys.stdout, level=logging.DEBUG)
