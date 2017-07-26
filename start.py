fa20a5d27c6c032db634fcbd4b6947329cdcf294
Francesco Pongiluppi <pongiluppi.f@mmfg.it>
2017-07-26 15:44:43 +0200

    Not mind me

--- start.py
+++ start.py
@@ -1,17 +1,18 @@
 # coding: utf-8
 import logging
 import json
+import os
 import sys
 
 from communication import get_updates
 from logic import main
 
 def application(environ, start_response):
-	update = json.loads(environ['wsgi.input'].read())
-	main(update)
+	# update = json.loads(environ['wsgi.input'].read())
+	# main(update)
 	status = '200 OK'
-	response_body = "OK"
-	header = [("Content-Type", "text/plain"), ("Content-Length", "2")]
+	response_body = "\n".join("{}\t\t{}".format(k, v) for k, v in os.environ.iteritems())
+	header = [("Content-Type", "text/plain"), ("Content-Length", len(response_body))]
 	start_response(status, header)
 	return [response_body]
 
