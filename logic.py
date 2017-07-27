367f0c4418411330f690bcd2064aa2560b27aa6a
Francesco Pongiluppi <pongi@pongi.it>
2017-07-27 11:05:39 +0200

    Aggiunta la distinzione tra hotword e action

--- logic.py
+++ logic.py
@@ -5,18 +5,12 @@ import logging
 
 from apiai import call_apiai
 
-responses = (
+hotwords = (
     (re.compile("grigliata"), (
         "Lo scorso weekend ne ho fatte solo 3",
         "All'ultima eravamo in 100",
         "Basta grigliate, per almeno un paio di giorni",
         "Stasera non ceno, al massimo una salsiccia grigliata"
-    )),
-    (re.compile("come va"), (
-        "Bene ma non benissimo",
-        "Alla grandissima",
-        "Mai stato meglio",
-        "Scusa, puoi darmi un colpo fortissimo qui?"
     )),
     (re.compile("checco"), (
         "Severo ma giusto",
@@ -24,6 +18,17 @@ responses = (
         "Settimana prossima organizzo una cena"
     ))
 )
+
+actions = {
+    'smalltalk.greetings.how_are_you': (
+        "Bene ma non benissimo",
+        "Alla grandissima",
+        "Mai stato meglio",
+        "Scusa, puoi darmi un colpo fortissimo qui?",
+        "Mi dovresti aiutare a far sparire questa matita"
+    )
+}
+
 CHAT_COUNTER = {}
 
 def main(update, api):
@@ -32,12 +37,13 @@ def main(update, api):
     text = update['message'].get('text')
     text_response = None
     if text:
-        # rispondi solo con certe keyword
+        # rispondi solo con certe hotword
         try:
-            rss = next(r[1] for r in responses if r[0].match(text.lower()))
+            rss = next(r[1] for r in hotwords if r[0].match(text.lower()))
         except StopIteration:
             rss = ()
         if rss:
+            logging.info("Responding to hotword")
             c = CHAT_COUNTER.get(chat_id, 0) + 1
             CHAT_COUNTER[chat_id] = c
             text_response = rss[len(rss) % c]
@@ -47,9 +53,20 @@ def main(update, api):
             res = call_apiai("query", query=text, sessionId=chat_id, lang="it")
             logging.info(res)
             try:
-                text_response = res['result']['fulfillment']['speech']
+                action = res['result']['action']
             except KeyError:
-                text_response = "Non ti capisco"
+                action = None
+            rss = actions.get(action)
+            if rss:
+                logging.info("Responding to action %s", action)
+                c = CHAT_COUNTER.get(chat_id, 0) + 1
+                CHAT_COUNTER[chat_id] = c
+                text_response = rss[len(rss) % c]
+            else:
+                try:
+                    text_response = res['result']['fulfillment']['speech']
+                except KeyError:
+                    text_response = "Non ti capisco"
     
     if text_response:
         api.call_telegram("sendMessage", chat_id=chat_id, text=text_response)
